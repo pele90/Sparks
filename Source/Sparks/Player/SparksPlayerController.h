2dd1833df19c24eda863399d3a1b83a4ae1d4cfd
Goran <goranpetkovicpele90@gmail.com>
2020-09-25 15:49:29 +0200

    Create basic classes and blueprints

---
+++ Source/Sparks/Player/SparksPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SparksPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPARKS_API ASparksPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Sparks/UI/SparksHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "SparksHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPARKS_API ASparksHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
